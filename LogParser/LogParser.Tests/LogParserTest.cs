210d8954e5fef94330c9eee97e79ec045431ca88
Tomas Bruckner <tomasbrucknermail@gmail.com>
2019-11-08 16:23:13 +0100

    init - working simple

---
+++ LogParser/LogParser.Tests/LogParserTest.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace LogParser.Tests
+{
+    public class LogParserTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var expected = new Dictionary<string, string>
+            {
+                {"temp-1", "precise"},
+                {"temp-2", "ultra precise"},
+                {"hum-1", "keep"},
+                {"hum-2", "discard"},
+                {"mon-1", "keep"},
+                {"mon-2", "discard"},
+            };
+            Assert.Equal(JsonConvert.SerializeObject(expected),
+                LogParser.Parse(
+                    @"reference 70.0 45.0 6
+thermometer temp-1
+2007-04-05T22:00 72.4
+2007-04-05T22:01 76.0
+2007-04-05T22:02 79.1
+2007-04-05T22:03 75.6
+2007-04-05T22:04 71.2
+2007-04-05T22:05 71.4
+2007-04-05T22:06 69.2
+2007-04-05T22:07 65.2
+2007-04-05T22:08 62.8
+2007-04-05T22:09 61.4
+2007-04-05T22:10 64.0
+2007-04-05T22:11 67.5
+2007-04-05T22:12 69.4
+thermometer temp-2
+2007-04-05T22:01 69.5
+2007-04-05T22:02 70.1
+2007-04-05T22:03 71.3
+2007-04-05T22:04 71.5
+2007-04-05T22:05 69.8
+humidity hum-1
+2007-04-05T22:04 45.2
+2007-04-05T22:05 45.3
+2007-04-05T22:06 45.1
+humidity hum-2
+2007-04-05T22:04 44.4
+2007-04-05T22:05 43.9
+2007-04-05T22:06 44.9
+2007-04-05T22:07 43.8
+2007-04-05T22:08 42.1
+monoxide mon-1
+2007-04-05T22:04 5
+2007-04-05T22:05 7
+2007-04-05T22:06 9
+monoxide mon-2
+2007-04-05T22:04 2
+2007-04-05T22:05 4
+2007-04-05T22:06 10
+2007-04-05T22:07 8
+2007-04-05T22:08 6"
+                )
+            );
+        }
+    }
+}
---
+++ LogParser/LogParser/Core/LogParser.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace LogParser
+{
+    public static class LogParser
+    {
+        private static readonly string[] X =
+        {
+            "thermometer", "humidity", "monoxide"
+        };
+
+
+        public static string Parse(string content)
+        {
+            using var sr = new StringReader(content);
+            var info = GetReferenceValues(sr.ReadLine());
+            var result = new Dictionary<string, string>();
+
+            (string, string) active = (null, null);
+            var value = new List<double>();
+            var line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                var parts = line.Split(' ');
+
+                if (X.Contains(parts[0]))
+                {
+                    if (active.Item1 != null)
+                    {
+                        var deviation = CalculateStdDev(value);
+                        if (active.Item1 == "thermometer")
+                        {
+                            result.Add(active.Item2, GetThermoValue(value.Average(), deviation, info["thermometer"]));
+                        }
+                        else if (active.Item1 == "humidity")
+                        {
+                            var reference = info["humidity"];
+                            var text = value.All(o => o >= (reference - 1) && o <= (reference + 1))
+                                ? "keep"
+                                : "discard";
+                            result.Add(active.Item2, text);
+                        }
+                        else
+                        {
+                            var reference = info["monoxide"];
+                            var text = value.All(o => o >= (reference - 3) && o <= (reference + 3))
+                                ? "keep"
+                                : "discard";
+                            result.Add(active.Item2, text);
+                        }
+                    }
+
+                    active = (parts[0], parts[1]);
+                    value.Clear();
+                }
+                else
+                {
+                    value.Add(double.Parse(parts[1], CultureInfo.InvariantCulture));
+                }
+            }
+
+            if (active.Item1 == "thermometer")
+            {
+                result.Add(active.Item2, GetThermoValue(value.Average(), CalculateStdDev(value), info["thermometer"]));
+            }
+            else if (active.Item1 == "humidity")
+            {
+                var reference = info["humidity"];
+                var text = value.All(o => o >= (reference - 1) && o <= (reference + 1))
+                    ? "keep"
+                    : "discard";
+                result.Add(active.Item2, text);
+            }
+            else
+            {
+                var reference = info["monoxide"];
+                var text = value.All(o => o >= (reference - 3) && o <= (reference + 3))
+                    ? "keep"
+                    : "discard";
+                result.Add(active.Item2, text);
+            }
+
+            return JsonConvert.SerializeObject(result);
+        }
+
+        private static Dictionary<string, double> GetReferenceValues(string line)
+        {
+            var x = line.Split(' ');
+
+            return new Dictionary<string, double>
+            {
+                {"thermometer", double.Parse(x[1], CultureInfo.InvariantCulture)},
+                {"humidity", double.Parse(x[2], CultureInfo.InvariantCulture)},
+                {"monoxide", double.Parse(x[3], CultureInfo.InvariantCulture)},
+            };
+        }
+
+        private static string GetThermoValue(double average, double deviation, double reference)
+        {
+            var isMean = average <= (reference + 0.5) && average >= (reference - 0.5);
+
+            if (deviation < 3 && isMean)
+            {
+                return "ultra precise";
+            }
+
+            if (deviation < 5 && isMean)
+            {
+                return "very precise";
+            }
+
+            return "precise";
+        }
+
+        private static double CalculateStdDev(IEnumerable<double> values)
+        {
+            var ret = 0.0;
+            var enumerable = values.ToList();
+            if (!enumerable.Any())
+            {
+                return ret;
+            }
+
+            var avg = enumerable.Average();
+            var sum = enumerable.Sum(d => Math.Pow(d - avg, 2));
+            ret = Math.Sqrt((sum) / (enumerable.Count() - 1));
+
+            return ret;
+        }
+    }
+}
